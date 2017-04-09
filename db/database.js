32f196cc1624c8c6765a2a8f642b77dc71875516
Elisha Pruner <elishapruner@gmail.com>
2017-04-09 19:02:00 -0400

    deleted database folder

--- db/database.js
+++
@@ -1,5 +0,0 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-
-const client = new pg.Client(connectionString);
-client.connect();
