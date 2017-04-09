486c66c9aba42e16f2fd0f36574f21646df639f1
Elisha Pruner <elishapruner@gmail.com>
2017-04-09 18:26:59 -0400

    changed view engine to ejs

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.ejs');
 });
 
 app.get('/user', function (req, res) {
-  res.sendFile(__dirname + '/public/user.html');
+  res.sendFile(__dirname + '/public/user.ejs');
 });
 
 app.get('/chef', function (req, res) {
-  res.sendFile(__dirname + '/public/chef.html');
+  res.sendFile(__dirname + '/public/chef.ejs');
 });
 
 app.get('/admin', function (req, res) {
-  res.sendFile(__dirname + '/public/admin.html');
+  res.sendFile(__dirname + '/public/admin.ejs');
 });
 
 app.listen(3000, function () {
