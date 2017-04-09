d3b4fb867dbcbfd0fd3e076d198202f9c8aee58a
Elisha Pruner <elishapruner@gmail.com>
2017-04-09 18:19:44 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
-})
+});
 
 app.get('/user', function (req, res) {
   res.sendFile(__dirname + '/public/user.html');
-})
+});
 
 app.get('/chef', function (req, res) {
   res.sendFile(__dirname + '/public/chef.html');
-})
+});
 
 app.get('/admin', function (req, res) {
   res.sendFile(__dirname + '/public/admin.html');
-})
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
