bfc159695b419f264c57511b3f4ea011ebeb9d41
Elisha Pruner <elishapruner@gmail.com>
2017-04-10 00:31:20 -0400

    GET commands now work on the user page

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 var express = require('express');
-// var pg = require('pg');
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
 var app = express();
 
-app.use(express.static('public'));
+var connectDB = "postgres://postgres:password@localhost/postgres";
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 app.get('/', function (req, res) {
-  res.render('index', {
-      title: 'hello world',
-      list: ['a', 'b']
-  });
+  res.render('index');
 });
 
 app.get('/user', function (req, res) {
-  res.render('user');
+  pg.connect(connectDB, function(err, client, done) {
+    if (err) {
+      return console.error('error in fetching client', err);
+    }
+
+    client.query('SELECT * FROM smartfridge.fooditem', function(err, food) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+      client.query('SELECT * FROM smartfridge.meal', function(err, meal) {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        res.render('user', {fooditem: food.rows, meal: meal.rows});
+      });
+      done();
+    });
+
+  });
 });
 
 app.get('/chef', function (req, res) {
@@ -25,6 +47,7 @@ app.get('/chef', function (req, res) {
 app.get('/admin', function (req, res) {
   res.render('admin');
 });
+
 
 app.listen(3000, function () {
   console.log('Running server on port 3000');
