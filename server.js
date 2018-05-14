5140fe9c3b290ed0d334f238387a5a11d5a4fd8c
Elisha Pruner <elishapruner@gmail.com>
2018-05-14 11:31:20 -0400

    updates

--- server.js
+++ server.js
@@ -83,36 +83,33 @@ app.post('/request_food', function (req, res) {
     if (err) {
       return console.error('error in fetching client', err);
     }
-    client.query('SELECT requestid FROM smartfridge.request ORDER BY requestid DESC LIMIT 1', function (err, requestID) {
+    client.query('SELECT foodreqid FROM smartfridge.foodrequest ORDER BY foodreqid DESC LIMIT 1', function (err, requestID) {
       if (err) {
         return console.error('error running query', err);
       }
-      var maxID = parseInt(requestID.rows[0].reqid);
-      console.log(maxID);
-      // client.query('INSERT INTO smartfridge.foodrequest(foodreqid, foodid, reguserid) VALUES($1, $2, $3)',
-      //   [maxID + 1, req.body.foodID, req.body.userID]);
-      // done();
-      // res.redirect('/user');
+      var maxID = parseInt(requestID.rows[0].foodreqid);
+      client.query('INSERT INTO smartfridge.foodrequest(foodreqid, foodid, reguserid) VALUES($1, $2, $3)',
+        [maxID + 1, req.body.foodID, req.body.userID]);
+      done();
     });
   });
 });
 
 app.post('/request_meal', function (req, res) {
-  // pg.connect(connectDB, function(err, client, done) {
-  //   if (err) {
-  //     return console.error('error in fetching client', err);
-  //   }
-  //   client.query('SELECT requestid FROM smartfridge.request ORDER BY requestid DESC LIMIT 1', function (err, requestID) {
-  //     if (err) {
-  //       return console.error('error running query', err);
-  //     }
-  //     var maxID = parseInt(requestID.rows[0].reqid);
-  //     client.query('INSERT INTO smartfridge.foodrequest(foodreqid, foodid, reguserid) VALUES($1, $2, $3)',
-  //       [maxID + 1, req.body.foodID, req.body.userID]);
-  //     done();
-  //     res.redirect('/user');
-  //   });
-  // });
+  pg.connect(connectDB, function(err, client, done) {
+    if (err) {
+      return console.error('error in fetching client', err);
+    }
+    client.query('SELECT mealreqid FROM smartfridge.mealrequest ORDER BY mealreqid DESC LIMIT 1', function (err, requestID) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+      var maxID = parseInt(requestID.rows[0].mealreqid);
+      client.query('INSERT INTO smartfridge.mealrequest(mealreqid, mealid, reguserid, chefid) VALUES($1, $2, $3, $4)',
+        [maxID + 1, req.body.mealID, req.body.userID, 1]);
+      done();
+    });
+  });
 });
 
 app.get('/chef', function (req, res) {
@@ -140,8 +137,8 @@ app.post('/meal_form', function (req, res) {
         return console.error('error running query', err);
       }
       var maxID = parseInt(mealID.rows[0].mealid);
-      client.query('INSERT INTO smartfridge.meal(mealid, name, cuisine, description) VALUES($1, $2, $3, $4)',
-        [maxID + 1, req.body.meal_name, req.body.meal_cuisine, req.body.meal_description]);
+      client.query('INSERT INTO smartfridge.meal(mealid, name, cuisine, description, ingredients) VALUES($1, $2, $3, $4, $5)',
+        [maxID + 1, req.body.meal_name, req.body.meal_cuisine, req.body.meal_description, req.body.meal_ingredient]);
       done();
       res.redirect('/chef');
     });
@@ -203,54 +200,71 @@ app.get('/admin', function (req, res) {
       if (err) {
         return console.error('error running query', err);
       }
-      res.render('admin', {ingredientorder: ingredient.rows});
-      done();
+      client.query('SELECT name FROM smartfridge.meal', function (err, meal) {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        res.render('admin', {ingredientorder: ingredient.rows, meal: meal.rows});
+        done();
+      });
     });
   });
 });
 
-app.post('/admin_place_order', function (req, res) {
-  // pg.connect(connectDB, function(err, client, done) {
-  //   if (err) {
-  //     return console.error('error in fetching client', err);
-  //   }
-  //   client.query('SELECT orderid FROM smartfridge.ingredientorder ORDER BY orderid DESC LIMIT 1', function (err, orderID) {
-  //     if (err) {
-  //       return console.error('error running query', err);
-  //     }
-  //     var maxID = parseInt(orderID.rows[0].orderid);
-  //     client.query('SELECT foodid FROM smartfridge.fooditem WHERE name=$1', [req.body.ingredient], function (err, foodid) {
-  //       if (err) {
-  //         return console.error('error running query', err);
-  //       }
-  //       var foodID = parseInt(foodid.rows[0].foodid);
-  //       client.query('INSERT INTO smartfridge.ingredientorder(orderid, foodid, chefid, adminid, approved) VALUES ($1, $2, $3, $4, $5)',
-  //         [maxID+1, foodID, req.body.chefID, 1, false]);
-  //     });
-  //     done();
-  //     res.redirect('/chef');
-  //   });
-  // });
+app.post('/approve', function (req, res) {
+  pg.connect(connectDB, function(err, client, done) {
+    if (err) {
+      return console.error('error in fetching client', err);
+    }
+    client.query('SELECT orderid, name, category, count, priceperitem, threshold, approved ' +
+      'FROM smartfridge.ingredientorder, smartfridge.fooditem ' +
+      'WHERE smartfridge.ingredientorder.foodid=smartfridge.fooditem.foodid', function(err, ingredient) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+      client.query('UPDATE smartfridge.ingredientorder SET approved=true where orderid=$1', [''+req.body.approve_orderID], function(err) {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        client.query('SELECT name FROM smartfridge.meal', function (err, meal) {
+          if (err) {
+            return console.error('error running query', err);
+          }
+          res.render('admin', {ingredientorder: ingredient.rows, meal: meal.rows});
+          done();
+        })
+      });
+      res.redirect('admin')
+    });
+  });
 });
 
-app.put('/accept', function (req, res) {
-  // pg.connect(connectDB, function(err, client, done) {
-  //   if (err) {
-  //     return console.error('error in fetching client', err);
-  //   }
-  //   client.query('DELETE FROM smartfridge.ingredientorder WHERE orderid=$1', [data-id]);
-  //   done();
-  // });
-});
-
-app.delete('/reject', function (req, res) {
-  // pg.connect(connectDB, function(err, client, done) {
-  //   if (err) {
-  //     return console.error('error in fetching client', err);
-  //   }
-  //   client.query('DELETE FROM smartfridge.ingredientorder WHERE orderid=$1', [data-id]);
-  //   done();
-  // });
+app.post('/delete', function (req, res) {
+  pg.connect(connectDB, function(err, client, done) {
+    if (err) {
+      return console.error('error in fetching client', err);
+    }
+    client.query('SELECT orderid, name, category, count, priceperitem, threshold, approved ' +
+      'FROM smartfridge.ingredientorder, smartfridge.fooditem ' +
+      'WHERE smartfridge.ingredientorder.foodid=smartfridge.fooditem.foodid', function(err, ingredient) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+      client.query('DELETE FROM smartfridge.ingredientorder where orderid=$1', [''+req.body.delete_orderID], function(err) {
+        if (err) {
+          return console.error('error running query', err);
+        }
+        client.query('SELECT name FROM smartfridge.meal', function (err, meal) {
+          if (err) {
+            return console.error('error running query', err);
+          }
+          res.render('admin', {ingredientorder: ingredient.rows, meal: meal.rows});
+          done();
+        });
+      });
+      res.redirect('admin')
+    });
+  });
 });
 
 app.listen(3000, function () {
